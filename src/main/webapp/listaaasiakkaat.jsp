b5ec1f78bc6b51effbfd888b4b9a69d41e9b91a6
Helene Öller <ac2080@student.jamk.fi>
2023-04-15 13:09:50 +0300

    backend

---
+++ src/main/webapp/listaaasiakkaat.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<script src="scripts/main.js"></script>
+<title>Asiakkaat</title>
+</head>
+<body>
+<table id="listaus">
+	<thead>		
+		<tr>
+			<th>Hakusana:</th>
+			<th colspan="3"><input type="text" id="hakusana"></th>
+			<th><input type="button" value="hae" id="hakunappi" onclick="haeAsiakkaat()"></th>
+		</tr>	
+		<tr>
+			<th>Etunimi</th>
+			<th>Sukunimi</th>
+			<th>Puhelin</th>
+			<th>Sposti</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody id="tbody">
+	</tbody>
+</table>
+<span id="ilmo"></span>
+<script>
+haeAsiakkaat();
+</script>
+</body>
+</html>
---
+++ src/main/webapp/scripts/main.js
@@ -0,0 +1,27 @@
+//funktio tietojen hakemista varten. Kutsutaan backin GET metodia
+function haeAsiakkaat() {
+	let url = "asiakkaat?hakusana=" + document.getElementById("hakusana").value; 
+	let requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }       
+    };    
+    fetch(url, requestOptions)
+    .then(response => response.json())//Muutetaan vastausteksti JSON-objektiksi 
+   	.then(response => printItems(response)) 
+   	.catch(errorText => console.error("Fetch failed: " + errorText));
+}
+
+//Kirjoitetaan tiedot taulukkoon JSON-objektilistasta
+function printItems(respObjList){
+	//console.log(respObjList);
+	let htmlStr="";
+	for(let item of respObjList){//yksi kokoelmalooppeista		
+    	htmlStr+="<tr id='rivi_"+item.asiakas_id+"'>";
+    	htmlStr+="<td>"+item.etunimi+"</td>";
+    	htmlStr+="<td>"+item.sukunimi+"</td>";
+    	htmlStr+="<td>"+item.puhelin+"</td>";
+    	htmlStr+="<td>"+item.sposti+"</td>";     	
+    	htmlStr+="</tr>";    	
+	}	
+	document.getElementById("tbody").innerHTML = htmlStr;	
+}
